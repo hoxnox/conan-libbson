41d46cb793c435145f1d05056e82a5b25c50d5ab
theirix <theirix@gmail.com>
2019-02-11 10:34:33 +0300

    Update to mongo-c-driver repository, version 1.13.1

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,16 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
-import os, shutil
+import os, shutil, fnmatch
 
 class LibbsonConan(ConanFile):
     name = "libbson"
-    version = "1.9.4"
+    version = "1.13.1"
     url = "https://github.com/theirix/conan-libbson"
     license = "Apache-2.0"
-    homepage = "https://github.com/mongodb/libbson"
+    homepage = "https://github.com/mongodb/mongo-c-driver"
     description = "A BSON utility library."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = {"shared": False}
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
@@ -20,77 +20,42 @@ class LibbsonConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def source(self):
-        tools.get("https://github.com/mongodb/libbson/releases/download/%s/libbson-%s.tar.gz"
+        tools.get("https://github.com/mongodb/mongo-c-driver/releases/download/%s/mongo-c-driver-%s.tar.gz"
                   % (self.version, self.version))
-        os.rename("libbson-%s" % self.version, self.source_subfolder)
+        os.rename("mongo-c-driver-%s" % self.version, self.source_subfolder)
         os.rename(os.path.join(self.source_subfolder, "CMakeLists.txt"),
                   os.path.join(self.source_subfolder, "CMakeListsOriginal.txt"))
         shutil.copy("CMakeLists.txt",
                     os.path.join(self.source_subfolder, "CMakeLists.txt"))
 
     def build(self):
-        # cmake is supported only for Visual Studio
-        use_cmake = self.settings.compiler == "Visual Studio"
-
-        if use_cmake:
-            cmake = CMake(self)
-            # upstream cmake is flawed and doesn't understand boolean values other than ON/OFF
-            cmake.definitions["ENABLE_STATIC"] = "OFF" if self.options.shared else "ON"
-            cmake.definitions["ENABLE_TESTS"] = False
-            cmake.definitions["CMAKE_INSTALL_PREFIX"] = ("%s/_inst" % self.build_folder)
-            cmake.verbose = True
-            cmake.configure(source_folder=self.source_subfolder)
-            cmake.build()
-            cmake.install()
-
-        else:
-
-            env_build = AutoToolsBuildEnvironment(self, win_bash=self.settings.os == 'Windows')
-
-            # compose configure options
-            prefix = os.path.abspath(os.path.join(self.build_folder, "_inst"))
-            if self.settings.os == 'Windows':
-                prefix = tools.unix_path(prefix)
-            configure_args = ['--prefix=%s' % prefix]
-            if self.options.shared:
-                configure_args.extend(["--enable-shared", "--disable-static"])
-            else:
-                configure_args.extend(["--disable-shared", "--enable-static"])
-            configure_args.extend(["--enable-examples=no", "--enable-tests=no"])
-
-            with tools.chdir(self.source_subfolder):
-                # refresh configure
-                self.run('autoreconf --force --verbose --install -I build/autotools',
-                         win_bash=self.settings.os == 'Windows')
-
-                # disable rpath build
-                tools.replace_in_file("configure", r"-install_name \$rpath/", "-install_name ")
-
-                env_build.configure(args=configure_args)
-
-                env_build.make(args=['install'])
-
+        cmake = CMake(self)
+        # upstream cmake is flawed and doesn't understand boolean values other than ON/OFF
+        cmake.definitions["ENABLE_STATIC"] = "OFF" if self.options.shared else "ON"
+        cmake.definitions["ENABLE_BSON"] = "ON"
+        cmake.definitions["ENABLE_TESTS"] = "OFF"
+        cmake.definitions["ENABLE_EXAMPLES"] = "OFF"
+        cmake.definitions["ENABLE_MONGOC"] = "OFF"
+        cmake.definitions["ENABLE_SNAPPY"] = "OFF"
+        cmake.definitions["ENABLE_ZLIB"] = "OFF"
+        cmake.definitions["ENABLE_UNINSTALL"] = "OFF"
+        cmake.configure(source_folder=self.source_subfolder)
+        cmake.build()
+        cmake.install()
 
     def package(self):
         self.copy("copying*", src=self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
-        self.copy(pattern="*.h", dst="include", src="_inst/include", keep_path=True)
-        # autotools has a bug on mingw: it does not copy bson-stdint.h so copy it manually
-        if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
-            self.copy(pattern="bson-stdint.h", dst=os.path.join("include", "libbson-1.0"),
-                      src=os.path.join(self.source_subfolder, "build", "cmake", "bson"), keep_path=False)
-        if self.options.shared:
-            if self.settings.os == "Macos":
-                self.copy(pattern="*.dylib", src="_inst/lib", dst="lib", keep_path=False)
-            elif self.settings.os == "Windows":
-                self.copy(pattern="*.dll*", src="_inst/bin", dst="bin", keep_path=False)
-                # mingw dll import libraries: libbson*.dll.a
-                self.copy(pattern="*bson*.dll.a", src="_inst/lib", dst="lib", keep_path=False)
-            else:
-                self.copy(pattern="*.so*", src="_inst/lib", dst="lib", keep_path=False)
-        else:
-            self.copy(pattern="*bson*.a", src="_inst/lib", dst="lib", keep_path=False, excludes='*dll*')
-        if self.settings.compiler == "Visual Studio":
-            self.copy(pattern="*.lib*", src="_inst/lib", dst="lib", keep_path=False)
+
+        if not self.options.shared:
+            # remove configs for shared libraries
+            shutil.rmtree(os.path.join(self.package_folder, 'lib', 'cmake', 'libbson-1.0'), ignore_errors=True)
+            if os.path.exists(os.path.join(self.package_folder, 'lib', 'pkgconfig', 'libbson-1.0.pc')):
+                os.unlink(os.path.join(self.package_folder, 'lib', 'pkgconfig', 'libbson-1.0.pc'))
+            # remove shared libraries
+            for root, _, filenames in os.walk(self.package_folder):
+                for pattern in ['*.so*', '*.dylib*']:
+                    for filename in fnmatch.filter(filenames, pattern):
+                        os.unlink(os.path.join(root, filename))
 
     def package_info(self):
         if self.options.shared:
