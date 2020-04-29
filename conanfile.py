39eb2813c0a24abb5af8aa6184de096e78388771
theirix <theirix@gmail.com>
2020-04-29 17:09:08 +0300

    Fix import order

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
 import os, shutil, fnmatch
+from conans import ConanFile, CMake, tools
 
 class LibbsonConan(ConanFile):
     name = "libbson"
